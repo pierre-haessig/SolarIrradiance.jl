11f566faed1bc0279b6539ae5e654dcc80a43a65
Pierre Haessig <pierre.haessig@crans.org>
2021-09-09 14:29:33 +0200

    Doc update + plot generation script

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 The SolarIrradiance Julia package implements computations about solar radiation
 on the surface of Earth.
-Applications include thermal modeling of buildings and Solar
+Applications include solar energy (solar panels) and building thermal modeling.
 
-In particular, it implements several methods from the litterature
+In particular, it implements several methods from the literature
 to transpose global irradiance data measured on an *horizontal* plane (GHI)
 to an irradiance on an *inclined* plane (like a solar panel or the wall of a building).
 
@@ -35,17 +35,17 @@ julia> using SolarIrradiance
 
 Assuming we have the following vector of Global Horizontal Irradiance (GHI) data:
 ```
-julia> GHI_day = [0.0, 0.0, 0.0, 0.0, 0.0, 39.18, 209.0, 374.0, 240.0, 476.0, 379.0, 619.0, 872.01, 664.0, 680.0, 434.0, 311.0, 313.0, 169.0, 20.88, 0.0, 0.0, 0.0, 0.0];
+julia> GHI_day = [0.0, 0.0, 0.0, 0.0, 0.0, 64.0, 218.0, 396.0, 566.0, 709.0, 827.01, 892.01, 851.01, 834.01, 756.01, 666.0, 519.0, 346.0, 168.0, 21.88, 0.0, 0.0, 0.0, 0.0];
 ```
 
-You can transform it to an irradiance on an inclined panel with the `global_radiation_tilt` function.
+We can transform it to an irradiance on an inclined panel with the `global_radiation_tilt` function.
 It requires inputs which specify both the GHI measurement to be transposed, the time and location of this measurement, and finally the orientation of the plane for the transposition. These parameters can be set as:
 
 ```julia
-# Time of the measurement
-n = 136 # can be computed with dayofyear(2012,05,15)
-tc = 0:23
-dt = 1/60 # 1 minute ~ instantaneous (0.0 is orbidden)
+# Time of the measurement (May 16, 2012)
+n = 137 # can be computed with dayofyear(2012, 05, 16)
+tc_range = 0:23
+dt = 1/60 # 1 minute ~ instantaneous (0.0 is forbidden)
 
 # Location (Rennes, France):
 lat = 48.117 #°
@@ -57,15 +57,24 @@ azimuth = 90 #° → west-facing
 albedo = 0.5; # in [0,1]
 ```
 
-Then, we can tranpose each of the 24 values in a loop:
+Then, we can transpose each of the 24 values in a loop:
 
-```
-Gplane = zeros(length(tc))
+```julia
+Gplane = zeros(length(tc_range))
 
-for (k, tck) in enumerate(tc)
+for (k, tck) in enumerate(tc_range)
     GHIk = GHI_day[k]
     Gplane[k] = global_radiation_tilt(GHIk, n, tck, dt, lat, lon, slope, azimuth, albedo)
 end
 ```
+The effect of the west orientation is visible: the panel receives more irradiance in the afternon, but much less irradiance in the morning:
+
+![](doc/img/GHI_2012-137_trans_s40_a90.png)
+
+Such a radiation transposition model can be used for example to study the effect
+of the azimuth and the slope of a solar panel:
+
+![GHI transposition for a range of slopes](doc/img/GHI_2012-137_trans_a0_slope_range.png)
+![GHI transposition for a range of azimuths](doc/img/GHI_2012-137_trans_s40_azim_range.png)
 
 For a more detailed introduction, see [doc/Usage.ipynb](doc/Usage.ipynb).
---
+++ doc/doc_illustrations.jl
@@ -0,0 +1,179 @@
+# Illustrations for SolarIrradiance documentation
+# Copyright (c) 2021 Pierre Haessig
+
+using SolarIrradiance
+using PyPlot
+#pygui(true) # for interactive plot windows
+
+## Load GHI data ##
+
+# 2012, day 136: sun & clouds
+year = 2012
+n = 136
+GHI_day = [0.0, 0.0, 0.0, 0.0, 0.0, 39.18, 209.0, 374.0, 240.0, 476.0, 379.0, 619.0, 872.01, 664.0, 680.0, 434.0, 311.0, 313.0, 169.0, 20.88, 0.0, 0.0, 0.0, 0.0]
+
+# 2012, day 137: sun
+n = 137
+GHI_day = [0.0, 0.0, 0.0, 0.0, 0.0, 64.0, 218.0, 396.0, 566.0, 709.0, 827.01, 892.01, 851.01, 834.01, 756.01, 666.0, 519.0, 346.0, 168.0, 21.88, 0.0, 0.0, 0.0, 0.0]
+
+
+## Custom colormaps for sets of license
+
+# https://gka.github.io/palettes/#/9|s|0030dd,ef272d|ffffe0,ff005e,93003a|1|1
+colorsBR = ["#0030dd", "#622cc7", "#8628b1", "#9f259b", "#b32486", "#c52370", "#d4235b", "#e22545", "#ef272d"]
+cmapBR = matplotlib.colors.LinearSegmentedColormap.from_list("BlueRed", colorsBR)
+
+# https://gka.github.io/palettes/#/9|s|3b2fa7,007300,887600,eb212a|ffffe0,ff005e,93003a|0|0
+colorsBGR = ["#3b2fa7", "#254968", "#0f622a", "#117300", "#447400", "#777600", "#a1610b", "#c6411a", "#eb212a"]
+cmapBGR = matplotlib.colors.LinearSegmentedColormap.from_list("BlueGreenRed", colorsBGR)
+
+# https://gka.github.io/palettes/#/9|d|0030dd,4d4d4d|4d4d4d,df0822|0|0
+colorsBKR = ["#0030dd", "#1337b9", "#263e95", "#3a4671", "#4d4d4d", "#713c42", "#962a37", "#ba192d", "#df0822"]
+# https://gka.github.io/palettes/#/9|d|0024cf,505050|505050,df0822|0|1
+colorsBKR = ["#0024cf", "#3d31ae", "#4d3d8e", "#52476f", "#505050", "#794c45", "#9c443a", "#be342e", "#df0822"]
+cmapBKR = matplotlib.colors.LinearSegmentedColormap.from_list("BlueGrayRed", colorsBKR)
+
+# https://gka.github.io/palettes/#/9|d|00aa00,b60053|b60053,0000aa|0|1
+colorsGRB = ["#00aa00", "#659129", "#89743b", "#a25148", "#b60053", "#9e0069", "#82007e", "#5e0094", "#0000aa"]
+cmapGRB = matplotlib.colors.LinearSegmentedColormap.from_list("GreenRedBlue", colorsGRB)
+
+
+## Transpose GHI for a given slope & azimuth ##
+
+# Time of the measurement
+tc_range = 0:23
+dt = 1/60 # 1 minute ~ instantaneous (0.0 is orbidden)
+
+# Location (Rennes, France):
+lat = 48.117 #°
+lon = -1.678 #°
+
+# Panel orientation:
+slope = 40 #°
+azimuth = 90 #° → west-facing
+albedo = 0.5; # in [0,1]
+
+Gplane = zeros(length(tc_range))
+
+for (k, tck) in enumerate(tc_range)
+    GHIk = GHI_day[k]
+    Gplane[k] = global_radiation_tilt(GHIk, n, tck, dt, lat, lon, slope, azimuth, albedo)
+end
+
+## Plot the single transpose output ##
+
+fig, ax = subplots(figsize=(5,3.5))
+ax.plot(tc_range, GHI_day, "o-", label="horizontal (data)")
+ax.plot(tc_range, Gplane, "D-", label="tilted (estimate)")
+ax.grid(true)
+ax.set(
+    title="Irradiance transposition\non a west-facing plane tilted by $(slope)°",
+    xlabel="time (UTC hours)",
+    ylabel="Irradiance (W/m²)",
+)
+ax.legend()
+fig.tight_layout()
+fname = "GHI_$(year)-$(n)_trans_s$(slope)_a$(azimuth).png"
+fig.savefig(fname, dpi=300)
+
+## Plot a range of azimuths ##
+
+azi_range = -90:15:90
+
+Gplane_azi = zeros(length(tc_range), length(azi_range))
+
+for (i, azimuth) in enumerate(azi_range)
+    for (k, tck) in enumerate(tc_range)
+        GHIk = GHI_day[k]
+        Gplane_azi[k,i] = global_radiation_tilt(GHIk, n, tck, dt, lat, lon, slope, azimuth, albedo)
+    end
+end
+
+# Plot azimuth range
+fig, ax = subplots(figsize=(5,3.5))
+
+ax.plot(tc_range, GHI_day, "-", label="GHI data",
+        color=(0,0.7,0, 0.5), ms=3, lw=3, alpha=0.5)
+
+for (i, azimuth) in enumerate(azi_range)
+    lw=1
+    ls="-"
+    if azimuth == -90
+        label = "−90° (E)"
+    elseif azimuth == 0
+        label = "0° (S)"
+    elseif azimuth == +90
+        label = "+90° (W)"
+    else
+        label = ""
+        lw=0.5
+        ls="-"
+    end
+    color = cmapBKR((i-1)/(length(azi_range)-1))
+    ax.plot(tc_range, Gplane_azi[:,i], ls, ms=3,
+            color = color, label=label, lw=lw)
+end
+
+ax.grid(true)
+ax.set(
+    title="Irradiance transposition\non a plane tilted by $(slope)° with varying azimuths",
+    xlabel="time (UTC hours)",
+    ylabel="Irradiance (W/m²)",
+)
+ax.set_ylim(0, 1400)
+ax.legend(ncol=2)
+fig.tight_layout()
+fname = "GHI_$(year)-$(n)_trans_s$(slope)_azim_range.png"
+fig.savefig(fname, dpi=300)
+
+
+## Plot a range of slopes ##
+
+azimuth = 0 #°
+slo_range = 0:15:90
+
+Gplane_slo = zeros(length(tc_range), length(slo_range))
+
+for (i, slope) in enumerate(slo_range)
+    for (k, tck) in enumerate(tc_range)
+        GHIk = GHI_day[k]
+        Gplane_slo[k,i] = global_radiation_tilt(GHIk, n, tck, dt, lat, lon, slope, azimuth, albedo)
+    end
+end
+
+# Plot slope range
+fig, ax = subplots(figsize=(5,3.5))
+
+ax.plot(tc_range, GHI_day, "-", label="GHI data",
+        color=(0,0.7,0, 0.5), ms=3, lw=3, alpha=0.5)
+
+for (i, slope) in enumerate(slo_range)
+    lw=1
+    ls="-"
+    if slope == 0
+        label = "0°"
+    elseif slope == +45
+        label = "45°"
+    elseif slope == +90
+        label = "90°"
+    else
+        label = ""
+        lw=0.5
+        ls="-"
+    end
+    color = cmapGRB((i-1)/(length(slo_range)-1))
+    ax.plot(tc_range, Gplane_slo[:,i], ls, ms=3,
+            color = color, label=label, lw=lw)
+end
+
+ax.grid(true)
+ax.set(
+    title="Irradiance transposition\non a south-facing plane with varying slopes",
+    xlabel="time (UTC hours)",
+    ylabel="Irradiance (W/m²)",
+)
+ax.set_ylim(0, 1400)
+ax.legend(ncol=2, loc="upper center")
+fig.tight_layout()
+fname = "GHI_$(year)-$(n)_trans_a$(azimuth)_slope_range.png"
+fig.savefig(fname, dpi=300)
